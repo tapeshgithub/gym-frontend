b41408739904c0d6cb6fe46d971560c57a888c43
tapeshgithub <152116266+tapeshgithub@users.noreply.github.com>
2026-05-29 15:51:04 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: "https://gym-production-25e0.up.railway.app",
   headers: { 'Content-Type': 'application/json' },
 });
 
